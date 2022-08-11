bc2a30bccaa691c90ef87e33d488d13e0f3d7a48
ToshaEX <tharinduoshan@gmail.com>
2022-08-11 12:12:29 +0530

    Rendering Movies in frontend

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {Box,CircularProgress,useMediaQuery,Typography} from "@mui/material"
+import { useSelector } from 'react-redux'
+import { useGetMoviesQuery } from '../../services/TMDB'
+import {MovieList} from './../index';
 
 const Movies = () => {
+
+  const {data,error,isFetching} =useGetMoviesQuery();
+   if(isFetching){
+    return(<Box display="flex" justifyContent="center">
+      <CircularProgress size="4rem"/>
+    </Box>)
+   }
+   if(!data.results.length){
+    return(<Box display="flex" alignItems="center" mt="20px">
+      <Typography variant='H4'>No movies that match that name.<br />
+      please search for something else</Typography>
+    </Box>)
+   }
+   if(error){
+    return("An error has occurred");
+   }
   return (
-    <div>Movies</div>
+    <div><MovieList movies={data}/></div>
   )
 }
 
