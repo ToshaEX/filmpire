c021c3d7daa68a3828a6c9aa87dca8e5c9bd91c6
ToshaEX <tharinduoshan@gmail.com>
2022-08-26 23:24:12 +0530

    Setup Movie Information

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import {Modal,Typography,Button,ButtonGroup,Grid,Box,CircularProgress,useMediaQuery,Rating} from "@mui/material"
+import {Movie as MovieIcon, Theaters, Language,PlusOne,Favorite, FavoriteBorderOutlined,Remove,ArrowBack} from "@mui/icons-material"
+import {Link,useParams} from "react-router-dom"
+import {useDispatch,useSelector} from "react-redux";
+import axios from "axios"
 
 const MovieInformation = () => {
-  return <div>MovieInformation</div>;
+  const {id} =useParams();
+
+ 
+  return <div>MovieInformation{id}</div>;
 };
 
 export default MovieInformation;
